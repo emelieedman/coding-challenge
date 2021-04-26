0bd0540c683b233e6a73edfb8f4cd7900f45a899
Emelie Edman <emelie@MacBook-Pro.local>
2021-04-26 19:55:11 +0200

    tidying up the code

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const txtInput = document.getElementById('txt');
 const rangeInput = document.getElementById('range');
 const displayNumberInPX = document.getElementById('range-output');
 const txtOutput = document.getElementById('output');
-const txtContainer = document.getElementById('output-wrapper');
+const txtContainer = document.getElementById('txtContainer');
 
 const onChangingInputText = () => {
   output.innerHTML = txtInput.value;
