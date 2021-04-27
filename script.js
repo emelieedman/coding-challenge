98e939aed76c2c1bc0b5c726bdb1f9bcfda3d777
Emelie Edman <emelie@MacBook-Pro.local>
2021-04-27 08:52:39 +0200

    added prettier config file and clarified the fitText function

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
-const txtInput = document.getElementById('txt');
-const rangeInput = document.getElementById('range');
-const displayNumber = document.getElementById('range-output');
-const txtOutput = document.getElementById('output');
-const txtContainer = document.getElementById('txtContainer');
+const txtInput = document.getElementById('txt')
+const rangeInput = document.getElementById('range')
+const displayNumber = document.getElementById('range-output')
+const txtOutput = document.getElementById('output')
+const txtContainer = document.getElementById('txtContainer')
 
 const onChangingInputText = () => {
-  txtOutput.innerHTML = txtInput.value;
-  fitText(rangeInput.value);
-};
+  txtOutput.innerHTML = txtInput.value
+  fitText(rangeInput.value)
+}
 
 const fitText = (containerWidth, fontSize = 48) => {
-  txtOutput.style.fontSize = fontSize + 'px';
+  txtOutput.style.fontSize = fontSize + 'px'
+  let currentContainerWidth = containerWidth
 
   while (txtOutput.offsetWidth > txtContainer.offsetWidth) {
-    txtOutput.style.fontSize = --containerWidth + 'px';
+    currentContainerWidth = currentContainerWidth - 1
+    txtOutput.style.fontSize = currentContainerWidth + 'px'
   }
-};
+}
 
 const onChangingContainerWidth = () => {
-  let containerWidth = rangeInput.value;
+  let containerWidth = rangeInput.value
 
-  displayNumber.innerHTML = containerWidth + 'px';
+  displayNumber.innerHTML = containerWidth + 'px'
 
-  txtContainer.style.width = containerWidth + 'px';
+  txtContainer.style.width = containerWidth + 'px'
 
-  fitText(containerWidth);
-};
+  fitText(containerWidth)
+}
